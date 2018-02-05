a6ba944b65983c2ffb88a9549e9579b4e3d4624c
MSI\bushi <tonywu@justek>
2018-02-05 21:10:11 +0800

    add movies header

---
+++ app/src/main/java/com/tony/dbmovie/view/widget/RateLayout.java
@@ -0,0 +1,23 @@
+package com.tony.dbmovie.view.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by bushi on 2018/2/3.
+ */
+
+public class RateLayout extends View {
+    public RateLayout(Context context) {
+        super(context);
+    }
+
+    private void initLayout()
+    {
+
+
+    }
+}
